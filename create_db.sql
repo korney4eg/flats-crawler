06c7ab52add9f5575c6eaa6c7d09092bb95d26c8
Alexey Korneev <korneevayu@gmail.com>
2015-01-27 00:41:27 +0300

    updated db_creation file

--- create_db.sql
+++ create_db.sql
@@ -1,26 +1,20 @@
--- creating database
 CREATE DATABASE IF NOT EXISTS flats;
-
-CREATE USER 'flat'@'localhost' IDENTIFIED BY 'flat';
-
-GRANT ALL PRIVILEGES ON flats.* TO 'flat'@'localhost';
-
+USE FLATS;
 DROP TABLE IF EXISTS `global`;
 CREATE TABLE `global` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `code` int(11) NOT NULL,
   `address` varchar(70) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `rooms` varchar(3) DEFAULT NULL,
   `year` int(11) DEFAULT NULL,
-  PRIMARY KEY (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
 DROP TABLE IF EXISTS `price_history`;
 CREATE TABLE `price_history` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `code` int(11) NOT NULL,
   `price` int(11) DEFAULT NULL,
   `date` date DEFAULT NULL,
-  PRIMARY KEY (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
