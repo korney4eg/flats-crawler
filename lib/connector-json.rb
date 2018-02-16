814a51364c57c4f7bc0bf8007919cd93c8e14bff
Aliaksei Karneyeu <aliaksei.karneyeu@grandparade.co.uk>
2018-02-16 15:04:38 +0100

    Add method to fix date in history

--- lib/connector-json.rb
+++ lib/connector-json.rb
@@ -1,9 +1,11 @@
 require 'json'
+require 'date'
 
 # DAO class for flats
 class JSONConnector
   def initialize( filename )
     @filename = filename
+    @current_date = Time.now.strftime("%Y-%m-%d")
     begin
       file = File.read(@filename)
       @data = JSON.parse(file)
@@ -24,11 +26,10 @@ class JSONConnector
     @data['flats'][code]['status'] = ''
     @data['flats'][code]['history'] = {}
 
-    time = Time.now
-    add_flat_hist(code, price,"#{time.year}-#{time.month}-#{time.day}" )
+    add_flat_hist(code, price, @current_date)
   end
 
-  def add_flat_hist(code, price, date ="#{Time.now.year}-#{Time.now.month}-#{Time.now.day}" )
+  def add_flat_hist(code, price, date = @current_date )
     @data['flats'][code]['history'][ date ] = price
   end
 
@@ -68,8 +69,7 @@ class JSONConnector
 
   def update_flat(code, price)
     @data['flats'][code]['price'] = price
-    time = Time.now
-    add_flat_hist(code,price,"#{time.year}-#{time.month}-#{time.day}")
+    add_flat_hist(code,price, @current_date)
   end
 
   def update_area(code, area)
@@ -80,10 +80,9 @@ class JSONConnector
   end
 
   def mark_sold(code)
-    time = Time.now
     @data['flats'][code]['status'] = 'sold'
     if !@data['flats'][code]['sold_date']
-      @data['flats'][code]['sold_date'] = "#{time.year}-#{time.month}-#{time.day}"
+      @data['flats'][code]['sold_date'] = @current_date
       return true
     else
       return false
@@ -93,6 +92,20 @@ class JSONConnector
   def mark_unsold(code)
     @data['flats'][code].delete('sold_date')
     @data['flats'][code].delete('status')
+  end
+
+  def fix_history_date_format
+    @data['flats'].each_pair do |code,flat|
+      temp_flat_hist = {}
+      flat['history'].each_key do |flat_date|
+        temp_flat_hist[Date::strptime(flat_date, "%Y-%m-%d")] = flat['history'][flat_date]
+        puts "converted #{flat_date} ---> #{Date::strptime(flat_date, "%Y-%m-%d")}"
+      end
+      flat['history'] = temp_flat_hist
+      if @data['flats'][code]['sold_date']
+        @data['flats'][code]['sold_date'] = Date::strptime(@data['flats'][code]['sold_date'], "%Y-%m-%d")
+      end
+    end
   end
 
   def close
